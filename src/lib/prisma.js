3190818c79add913a7431aaedfc96c8fb2c52bcb
ballpen <gomboy03@gmail.com>
2026-02-08 22:23:29 +0900

    fix: DB 연결 예외처리

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -4,7 +4,7 @@ const { PrismaPg } = require('@prisma/adapter-pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: true },
+  ssl: { rejectUnauthorized: false },
 })
 
 const adapter = new PrismaPg(pool)
